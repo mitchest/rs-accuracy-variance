66804eabc2eb2ca786438743ab7ed1e77a3b8cce
Mitchell Lyons <mitchell.lyons@gmail.com>
2017-05-25 12:48:27 +1000

    catch em all

--- accuracy_functions.R
+++ accuracy_functions.R
@@ -1,3 +1,48 @@
 source_lines <- function(file, lines){
   source(textConnection(readLines(file)[lines]))
 }
+
+percentage_agreement <- function(reps, true_id, pred_class, data) {
+  sum(as.character(data[true_id[[reps]], "veg_cl_tm"]) == as.character(pred_class[[reps]])) / length(pred_class[[reps]])
+}
+
+collect_pa_results <- function(this_row, get_this, iter_n, data) {
+  if (get_this$type[this_row] == "boot") {
+    return(
+      data.frame(
+        perc_agr = unlist(
+          lapply(
+            X = 1:length(iter_n[["boot"]][[1]]),
+            FUN = percentage_agreement,
+            iter_n[["boot"]][[get_this$tt[this_row]]],
+            iter_n[["boot"]][[get_this$method[this_row]]],
+            data)),
+        type = get_this$type[this_row],
+        method = get_this$method[this_row])
+    )
+  } else {
+    return(
+      data.frame(
+        perc_agr = unlist(
+          lapply(
+            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
+            FUN = percentage_agreement,
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
+            data)),
+        type = get_this$type[this_row],
+        method = get_this$method[this_row])
+    )
+  }
+}
+
+collect_pa_iteration <- function(iter_n, get_this, big_list, data) {
+  print(paste0("Collecting iteration ", iter_n))
+  rbindlist(lapply(
+    X = 1:nrow(get_this),
+    FUN = collect_pa_results,
+    get_this,
+    big_list[[iter_n]],
+    data
+  )) %>% mutate(iter_n = iter_n)
+}
--- calculate_accuracy.R
+++ calculate_accuracy.R
@@ -5,6 +5,7 @@ source("accuracy_functions.R")
 
 # get same data as used for allocations
 source_lines("calculate_allocations.R", 10:21) # careful!
+rm(survey_points_raw)
 
 
 # load("big_list.RData")
@@ -23,57 +24,18 @@ get_this <- data.frame(scenario = c("boot", "boot", rep("rrcv", 12), rep("kfold"
                        stringsAsFactors = F)
 
 
+
 # calculate stats ---------------------------------------------------------
 
+pa_results <- rbindlist(lapply(
+  X = 1:length(big_list),
+  FUN = collect_pa_iteration,
+  get_this, big_list, survey_points
+))
 
-percentage_agreement <- function(reps, true_id, pred_class, data) {
-  sum(as.character(data[true_id[[reps]], "veg_cl_tm"]) == as.character(pred_class[[reps]])) / length(pred_class[[reps]])
-}
 
 
-collect_pa_results <- function(this_row, get_this, iter_n, data) {
-  if (get_this$type[this_row] == "boot") {
-    return(
-      data.frame(
-        perc_agr = unlist(
-          lapply(
-            X = 1:length(iter_n[["boot"]][[1]]),
-            FUN = percentage_agreement,
-            iter_n[["boot"]][[get_this$tt[this_row]]],
-            iter_n[["boot"]][[get_this$method[this_row]]],
-            data)),
-        type = get_this$type[this_row],
-        method = get_this$method[this_row])
-    )
-  } else {
-    return(
-      data.frame(
-        perc_agr = unlist(
-          lapply(
-            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
-            FUN = percentage_agreement,
-            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
-            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
-            data)),
-        type = get_this$type[this_row],
-        method = get_this$method[this_row])
-    )
-  }
-}
+# plots -------------------------------------------------------------------
 
-test = bind_rows(lapply(X = 1:nrow(get_this),
-                  FUN = collect_pa_results,
-                  get_this, iter_n, survey_points))
 
-collect_one_iteration <- function(X) {
-  
-}
-  
-  
-  
-  
-  
-  
-  
-  
 
