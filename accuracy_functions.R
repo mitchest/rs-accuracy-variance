0da78730b40c679a78af6b972ec908c271543b4a
Mitchell Lyons <mitchell.lyons@gmail.com>
2017-05-30 15:42:18 +1000

    add allocation/quantity disagreement; change collection to all metrics

--- accuracy_functions.R
+++ accuracy_functions.R
@@ -2,14 +2,22 @@ source_lines <- function(file, lines){
   source(textConnection(readLines(file)[lines]))
 }
 
+
+
+# metrics -----------------------------------------------------------------
+
+get_conf_mat <- function(reps, true_id, pred_class, data) {
+  table(as.character(pred_class[[reps]]),
+        as.character(data[true_id[[reps]], "veg_cl_tm"]))
+}
+
 percentage_agreement <- function(reps, true_id, pred_class, data) {
   sum(as.character(data[true_id[[reps]], "veg_cl_tm"]) == as.character(pred_class[[reps]])) / length(pred_class[[reps]])
 }
 
 # entropy and purity stolen from {IntNMF} package
 entropy <- function(reps, true_id, pred_class, data) {
-  conf_mat <- table(as.character(pred_class[[reps]]),
-                    as.character(data[true_id[[reps]], "veg_cl_tm"]))
+  conf_mat <- get_conf_mat(reps, true_id, pred_class, data)
   inner_sum <- apply(conf_mat, 1, function(x) {
     c_size <- sum(x)
     sum(x * ifelse(x != 0, log2(x/c_size), 0))
@@ -18,13 +26,34 @@ entropy <- function(reps, true_id, pred_class, data) {
 }
 
 purity <- function(reps, true_id, pred_class, data) {
-  sum(apply(
-    table(as.character(pred_class[[reps]]),
-          as.character(data[true_id[[reps]], "veg_cl_tm"])),
-    1, max)) / length(pred_class[[reps]])
+  sum(apply(get_conf_mat(reps, true_id, pred_class, data), 1, max)) / length(pred_class[[reps]])
 }
 
-collect_pa_results <- function(this_row, get_this, iter_n, data) {
+# disagreemetns kind of stolen from {diffR} package
+disagreement <- function(reps, true_id, pred_class, data) {
+  conf_mat <- get_conf_mat(reps, true_id, pred_class, data)
+  #if(!all(dim(test_mat) == 4)) {return(NA)}
+  1 - (sum(diag(conf_mat)) / sum(conf_mat))
+}
+
+quantity_disagreement <- function(reps, true_id, pred_class, data) {
+  conf_mat <- get_conf_mat(reps, true_id, pred_class, data)
+  #if(!all(dim(test_mat) == 4)) {return(NA)}
+  sum(abs(apply(conf_mat, 1, sum) - apply(conf_mat, 2, sum))) / 2 / sum(conf_mat)
+}
+
+allocation_disagreement <- function(reps, true_id, pred_class, data) {
+  #if(!all(dim(test_mat) == 4)) {return(NA)}
+  conf_mat <- get_conf_mat(reps, true_id, pred_class, data)
+  disagreement(reps, true_id, pred_class, data) - quantity_disagreement(reps, true_id, pred_class, data)
+}
+
+
+
+# collect metrics ---------------------------------------------------------
+
+collect_metric_results <- function(this_row, get_this, iter_n, data) {
+  print(get_this[this_row,])
   if (get_this$type[this_row] == "boot") {
     return(
       data.frame(
@@ -32,6 +61,34 @@ collect_pa_results <- function(this_row, get_this, iter_n, data) {
           lapply(
             X = 1:length(iter_n[["boot"]][[1]]),
             FUN = percentage_agreement,
+            iter_n[["boot"]][[get_this$tt[this_row]]],
+            iter_n[["boot"]][[get_this$method[this_row]]],
+            data)),
+        entropy = unlist(
+          lapply(
+            X = 1:length(iter_n[["boot"]][[1]]),
+            FUN = entropy,
+            iter_n[["boot"]][[get_this$tt[this_row]]],
+            iter_n[["boot"]][[get_this$method[this_row]]],
+            data)),
+        purity = unlist(
+          lapply(
+            X = 1:length(iter_n[["boot"]][[1]]),
+            FUN = purity,
+            iter_n[["boot"]][[get_this$tt[this_row]]],
+            iter_n[["boot"]][[get_this$method[this_row]]],
+            data)),
+        quant_dis = unlist(
+          lapply(
+            X = 1:length(iter_n[["boot"]][[1]]),
+            FUN = quantity_disagreement,
+            iter_n[["boot"]][[get_this$tt[this_row]]],
+            iter_n[["boot"]][[get_this$method[this_row]]],
+            data)),
+        alloc_dis = unlist(
+          lapply(
+            X = 1:length(iter_n[["boot"]][[1]]),
+            FUN = allocation_disagreement,
             iter_n[["boot"]][[get_this$tt[this_row]]],
             iter_n[["boot"]][[get_this$method[this_row]]],
             data)),
@@ -49,6 +106,34 @@ collect_pa_results <- function(this_row, get_this, iter_n, data) {
             iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
             iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
             data)),
+        entropy = unlist(
+          lapply(
+            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
+            FUN = entropy,
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
+            data)),
+        purity = unlist(
+          lapply(
+            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
+            FUN = purity,
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
+            data)),
+        quantity_disagreement = unlist(
+          lapply(
+            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
+            FUN = quantity_disagreement,
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
+            data)),
+        allocation_disagreement = unlist(
+          lapply(
+            X = 1:length(iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[1]]),
+            FUN = allocation_disagreement,
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$tt[this_row]]],
+            iter_n[[get_this$scenario[this_row]]][[get_this$type[this_row]]][[get_this$method[this_row]]],
+            data)),
         type = get_this$type[this_row],
         method = get_this$method[this_row],
         scenario = get_this$scenario[this_row])
@@ -56,18 +141,16 @@ collect_pa_results <- function(this_row, get_this, iter_n, data) {
   }
 }
 
-collect_pa_iteration <- function(iter_n, get_this, big_list, data) {
+collect_one_iteration <- function(iter_n, get_this, big_list, data) {
   print(paste0("Collecting iteration ", iter_n))
   rbindlist(lapply(
     X = 1:nrow(get_this),
-    FUN = collect_pa_results,
+    FUN = collect_metric_results,
     get_this,
     big_list[[iter_n]],
     data
   )) %>% mutate(iter_n = iter_n)
 }
-
-
 
 
 
--- calculate_accuracy.R
+++ calculate_accuracy.R
@@ -6,12 +6,14 @@ library(data.table)
 source("accuracy_functions.R")
 
 # get same data as used for allocations
-source_lines("calculate_allocations.R", 8:23) # careful!
+source_lines("calculate_allocations.R", 7:25) # careful!
 #rm(survey_points_raw)
 
 
 load("A:/1_UNSW/0_data/Dharawal_project/big_list.RData")
-#load("big_list_lite.RData") # temporary so development is more wieldly
+# big_list <- big_list[1:5]
+# save(big_list, file = "big_list.RData")
+# load("big_list.RData") # temporary so development is more wieldly
 
 # decide what results to extract - be VERY careful, and examine the data frame well
 get_this <- rbind(
@@ -48,9 +50,9 @@ get_this <- rbind(
 
 # calculate stats ---------------------------------------------------------
 
-pa_results <- rbindlist(lapply(
+metric_results <- rbindlist(lapply(
   X = 1:length(big_list),
-  FUN = collect_pa_iteration,
+  FUN = collect_one_iteration,
   get_this, big_list, survey_points
 ))
 
@@ -59,12 +61,20 @@ pa_results <- rbindlist(lapply(
 # plots -------------------------------------------------------------------
 
 # plot all data as boxplots
+metrics <- c("perc_agr","entropy","purity","quant_dis","alloc_dis")
+
+
+
+
+# old pa plot
 pa_plot <- ggplot(data = pa_results, aes(y = perc_agr)) +
   geom_boxplot(aes(x = type, colour = scenario, fill = method), notch = T) +
   scale_fill_manual(values = c("#fcbba1", "#fb6a4a", "#d4b9da", "#99d8c9", "#238b45")) +
   scale_colour_manual(values = c("#252525", "#e31a1c", "#3f007d"))
 ggsave("perc-agr_results.pdf", plot = pa_plot, device = "pdf", width = 10, height = 5)
 
+
+# test perc agreement on smaller chunks of iters and individual iters
 iter_n_breaks <- list(1:30, 31:60, 61:90, 91:120, 121:150, 151:180, 181:210, 211:240, 241:270, 271:300)
 pa_res_plots <- lapply(X = iter_n_breaks, FUN = plot_pa_results, pa_results)
 ggsave("perc-agr_results_30iters.pdf", plot = grid.arrange(grobs = pa_res_plots, ncol=2),
@@ -74,6 +84,10 @@ iter_n_breaks <- as.list(sample(1:300,10))
 pa_res_plots <- lapply(X = iter_n_breaks, FUN = plot_pa_results, pa_results)
 ggsave("perc-agr_results_indiv-iter.pdf", plot = grid.arrange(grobs = pa_res_plots, ncol=2),
        device = "pdf", width = 20, height = 10)
+
+
+
+
 
 # looks at mean/CI/min-max summaries
 pa_plotting <- pa_results %>%
